5227ae91ae8c40a0b7990cc4958ada1e906de894
lookprintufc <patrickmagal@alu.ufc.br>
2017-07-10 01:29:34 -0300

    s

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_egej17_novo_session', domain: :all
+Rails.application.config.session_store :cookie_store, key: '_efej17_session', domain: :all
 
