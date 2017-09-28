67afd915587c81db7a4812a4350800c87a94ee8d
rayane <rayanesantos-131@hotmail.com>
2017-09-28 17:37:53 -0300

    kaka

--- app/controllers/crew/schedules_controller.rb
+++ app/controllers/crew/schedules_controller.rb
@@ -18,19 +18,15 @@ class Crew::SchedulesController < Crew::BaseController
   # POST /events
   def create
     @schedule = @event.schedules.new(schedule_params)
-    if @schedule == !blank?
-    if @schedule.end_time_bigger?
-      if @schedule.save
-        redirect_to [:crew, @event], notice: 'Horário da programação criado com sucesso.'
-      else
-        render :new
+      respond_to do |format|
+        if @schedule.save
+          format.html { redirect_to crew_events_path, notice: 'Horário da programação criado com sucesso.' }
+          format.json { render :show, status: :created, location: @schedule }
+        else
+          format.html { render :new }
+          format.json { render json: @schedule.errors, status: :unprocessable_entity }
+        end
       end
-    else
-      redirect_to new_crew_event_schedule_path, notice: 'Horário de término da programação deve ser posterior ao horário de início.'
-    end
-    else
-      redirect_to new_crew_event_schedule_path, notice: 'Horário de início e término da programação não podem ficar em branco.'
-    end
   end
 
   # PATCH/PUT /events/1
