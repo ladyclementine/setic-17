9b4cb0a0d3e6123bf7734e2029c33d6d333dfabc
lookprintufc <patrickmagal@alu.ufc.br>
2017-10-02 17:41:06 -0300

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'www.efej.com.br' }
+  config.action_mailer.default_url_options = { :host => 'stem17.herokuapp.com' }
 
   Rails.application.config.middleware.use ExceptionNotification::Rack,
   :email => {
