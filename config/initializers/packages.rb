0a0e81672c91c86a178d736d8176d7427e8f85b1
rayane <rayanesantos-131@hotmail.com>
2017-10-09 19:14:43 -0300

    aws-config

--- config/initializers/packages.rb
+++ config/initializers/packages.rb
@@ -1,40 +1,52 @@
 module Packages
   ALL_PACKAGES = {
-    # pacote_1:  {
-    #   name: 'BRONZE',
-    #   types: {
-    #     "Palestra": 2,
-    #     "Mesa Redonda": 1,
-    #     "MiniCurso": 2
-    #   },
-    #   price: 20.0,
-    #   limit: 0
-    # },
-    # pacote_2:  {
-    #   name: 'PRATA',
-    #   types: {
-    #     "Palestra": 2,
-    #     "Mesa Redonda": 1,
-    #     "MiniCurso": 2,
-    #     "Visita Técnica": 1
+     pacote_1:  {
+       name: 'Pacote Bom',
+       types: {
+         "Palestra": 2,
+         "Mesa Redonda": 1,
+         "MiniCurso": 2
+       },
+       price: 20.0,
+       limit: 0
+     },
+     pacote_2:  {
+       name: 'Pacote Muito bom',
+       types: {
+         "Palestra": 2,
+         "Mesa Redonda": 1,
+         "MiniCurso": 2,
+         "Visita Técnica": 1
 
-    #   },
-    #   price: 30.0,
-    #   limit: 10
-    # },
-    # pacote_3:  {
-    #   name: 'OURO',
-    #   types: {
-    #     "Palestra": 2,
-    #     "Mesa Redonda": 1,
-    #     "MiniCurso": 2,
-    #     "Visita Técnica": 2,
-    #     "Oficina": 1
+       },
+       price: 30.0,
+       limit: 10
+     },
+     pacote_3:  {
+       name: 'Pacote Ótimo',
+       types: {
+         "Palestra": 2,
+         "Mesa Redonda": 1,
+         "MiniCurso": 2,
+         "Visita Técnica": 2,
+         "Oficina": 1
 
-    #   },
-    #   price: 40.0,
-    #   limit: 5
-    # }
+       },
+       price: 40.0,
+       limit: 5
+     }
+     pacote_3:  {
+       name: 'Pacote Excelente',
+       types: {
+         "Palestra": 2,
+         "Mesa Redonda": 1,
+         "MiniCurso": 2,
+         "Visita Técnica": 2,
+         "Oficina": 1
+
+       },
+       price: 40.0,
+       limit: 5
+     }
   }
 end
-
